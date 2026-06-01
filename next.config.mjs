db4206402202f77f6120550a97e24746753c7362
Wildan Yazid Ziddan <wildanziddan28@gmail.com>
2026-06-01 11:41:29 +0700

    chore: update next.config.mjs and package.json for improved build configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,25 @@
+// import createNextIntlPlugin from 'next-intl/plugin';
+
+// const withNextIntl = createNextIntlPlugin();
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default withNextIntl(nextConfig);
+
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 🔑 KUNCI SAKTI VERSI NEXT.JS 16
   typescript: {
-    // Memaksa Vercel tetep nyelesaiin build production meskipun banyak file demo bawaan template yang error
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true, // Libas error type file bawaan template
   },
   eslint: {
-    // Cuekin juga error ESLint pas di awan biar gak sensitif amat wkwk
-    ignoreDuringBuilds: true,
-  },
+    ignoreDuringBuilds: true, // Cuekin warning/error linting
+  }
 };
 
 export default withNextIntl(nextConfig);
