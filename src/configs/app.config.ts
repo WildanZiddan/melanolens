7d899e53d331fb11d03cb471387f62166205f16b
Wildan Yazid Ziddan <wildanziddan28@gmail.com>
2026-09-13 21:17:35 +0700

    chore: update backendApiUrl to Azure production URL

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -13,9 +13,7 @@ const appConfig: AppConfig = {
     unAuthenticatedEntryPath: '/sign-in',
     locale: 'en',
     activeNavTranslation: false,
-    // backendApiUrl: process.env.NEXT_PUBLIC_API_URL || 'https://melanolens-be-production.up.railway.app',
-    backendApiUrl: process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000',
-
+    backendApiUrl: process.env.NEXT_PUBLIC_API_URL || 'https://melanolens-be-b9hwazeycfayg9ee.indonesiacentral-01.azurewebsites.net',
 }
 
 export default appConfig
